9de35b070102dbaba43317358600281734dc77e1
devodog <dag@micronix.no>
2024-02-07 22:23:33 +0100

    Minimalistic stm32 driver operation for Hitachi Display Controller verified.

--- Nucleo-F302R8/bare-metal/Core/Src/lcd16x2.c
+++ Nucleo-F302R8/bare-metal/Core/Src/lcd16x2.c
@@ -23,27 +23,22 @@
    PB3 - D3 - SWO - CN4.6 (SWD header)
 
 
-   LCD pin 4 (RS) to PB6 --> PB0
+   LCD pin 4 (RS) to PB0 On NucleoF302R8 CN7[pin34]
    LCD pin 5 (R/W) (grounded)
-   LCD pin 6 (EN) to PB7 --> PB1
+   LCD pin 6 (EN) to PB1 On NucleoF302R8 CN10[pin24]
 
    LCD pin 7 (D0) to not used
    LCD pin 8 (D1) to not used
    LCD pin 9 (D2) to not used
    LCD pin 10 (D3) to not used
 
-   LCD pin 11 (D4) to PB0 --> PB4
-   LCD pin 12 (D5) to PB1 --> PB5
-   LCD pin 13 (D6) to PB2 --> PB6
-   LCD pin 14 (D7) to PB3 --> PB7
+   LCD pin 11 (D4) to PB4 On NucleoF302R8 CN10[pin27]
+   LCD pin 12 (D5) to PB5 On NucleoF302R8 CN10[pin29]
+   LCD pin 13 (D6) to PB6 On NucleoF302R8 CN10[pin17]
+   LCD pin 14 (D7) to PB7 On NucleoF302R8 CN7[pin21]
 
    LCD pin 15 (LED+) to 5V
    LCD pin 16 (LED-) to GND = RED Backlight
- *
- *
- *
- * See more at
- * https://
  *
  */
 #include "stdio.h"
@@ -56,45 +51,28 @@ int lcdInterfaceInit() {
    // As indicated by the header, we'll need to configure 4 GPIO registers to
    // deploy the necessary I/O pins for output operation.
    // Setting the mode register
-   // 01: General purpose output mode - for PB0 - PB3 & PB6 & PB7 the 32bit-map
-   // PB8 is used for test.
+   // 01: General purpose output mode - for PB4 - PB7 & PB0 & PB1 the 32bit-map
    // will look like this:
-   //                 B8B7B6B5B4B3B2B1B0
-   // 0b00000000000000010101000001010101 = 0x0015055
-   //GPIOB->MODER |= 0x15055UL;
-   // ----------------------------------------------
    //                   D7D6D5D4    E SR
    //                 B8B7B6B5B4    B1B0
    // 0b00000000000000010101010100000101 = 0x15505
-   //GPIOB->MODER |= 0x15500U;
+   GPIOB->MODER |= 0x15505U;
 
    // GPIO port output type: 0 = Output push-pull (reset state)
-   //GPIOB_OTYPER |= 0b0000000000000000;
+   GPIOB->OTYPER |= 0b0000000000000000;
 
    // GPIO port output speed
    // x0: Low speed
-
    // 01: Medium speed
-   //                 B8B7B6----B3B2B1B0
-   // 0b00000000000000010101000001010101 = 0x0015055
-   //GPIOB->OSPEEDR |= 0x15055UL;
-
    // 10: High speed
-   //                 B8B7B6----B3B2B1B0
-   // 0b00000000000000011010000010101010 = 0x1A0AA
-   //GPIOB->OSPEEDR |= 0x1A0AAUL;
-   // ----------------------------------------------
    //                   D7D6D5D4    E SR
    //                 B8B7B6B5B4    B1B0
    // 0b00000000000000011010101000001010 = 0x1AA0A
-   //GPIOB->OSPEEDR |= 0x1AA00U;
+   GPIOB->OSPEEDR |= 0x1AA00U;
 
    // GPIO port pull-up/pull-down register
    // 00: No pull-up, pull-down
-   //GPIOB->PUPDR |= 0b0000000000000000;
-/*** Left inside to indicate that there are many ways to access the GPIO output ports...
- * The GPIO is configured through the ioc-graphical user interface...
- **/
+   GPIOB->PUPDR |= 0b0000000000000000;
   return 0;
 }
 
@@ -106,43 +84,47 @@ void load4BitBus(uint8_t bits) {
 
 void pulseEnable() {
    GPIOB->ODR &= ~0x2; // Enable 0
-   delay_us(1);
    GPIOB->ODR |= 0x2; // Enable 1
-   delay_us(1);
+   delay_us(10);
    GPIOB->ODR &= ~0x2; // Enable 0
    delay_us(100);
 }
 
+/*
+ * lcd4wireHwInit() is to initialize the 4-wire instruction/data bus on the
+ * HD44780 display controller.
+ * It will load the instructions according to the recommended initialization
+ * sequence for the HD44780 display controller's.
+ *
+ */
 void lcd4wireHwInit() {
-   HAL_Delay(50); // Wait 50 ms after power is applied.
+   HAL_Delay(50); // Wait 50 ms after power is applied - could reduce this time
    // Clear the bits in the GPIO Port B that is used for communication with the
    // LCD, alike Hitachi HD44780 display controller.
    GPIOB->ODR &= ~0xf3; // RS = 0 (PB6) & EN = 0 (PB7) & D4-D7 = 0 (PB0-PB3)
 
-   load4BitBus(0x30); // Set D4-D7 = 0b0011 (PB4-PB7)
+   load4BitBus(0x30); // Set D4-D7 = 0b0011xxxx (PB4-PB7)
    pulseEnable();
    delay_us(4500);
-   //printf("\r\nODR=0x%x", (unsigned int)GPIOB->ODR);
 
-   load4BitBus(0x30); // Set D4-D7 = 0b0011 (PB4-PB7)
+   load4BitBus(0x30); // Set D4-D7 = 0b0011xxxx (PB4-PB7)
    pulseEnable();
    delay_us(4500);
-   //printf("\r\nODR=0x%x", (unsigned int)GPIOB->ODR);
-   load4BitBus(0x30); // Set D4-D7 = 0b0011 (PB4-PB7)
+
+   load4BitBus(0x30); // Set D4-D7 = 0b0011xxxx (PB4-PB7)
    pulseEnable();
    delay_us(150);
-   //printf("\r\nODR=0x%x", (unsigned int)GPIOB->ODR);
 
+   load4BitBus(0x20); // Set D4-D7 = 0b0010xxxx (PB4-PB7)
+   pulseEnable();
+   delay_us(150);
+
+   // Not sure that this is really needed...
+   /****
    load4BitBus(0x20); // Set D4-D7 = 0b0010 (PB4-PB7)
    pulseEnable();
    delay_us(150);
-   //printf("\r\nODR=0x%x", (unsigned int)GPIOB->ODR);
-   load4BitBus(0x20); // Set D4-D7 = 0b0010 (PB4-PB7)
-   pulseEnable();
-   delay_us(150);
-   //printf("\r\nODR=0x%x", (unsigned int)GPIOB->ODR);
-   // The 4-bit interface should now be operative...
-   //printf("\r\nODR=0x%x", GPIOB->ODR);
+   ***/
 }
 
 void loadLcdRegister(uint8_t regValue, uint8_t regType) {
@@ -152,31 +134,27 @@ void loadLcdRegister(uint8_t regValue, uint8_t regType) {
    // regType 1 = Data register
    if (regType == 1) {
       GPIOB->ODR |= 0x1;
-      printf("\r\nD=0x%x", (unsigned int)GPIOB->ODR);
+      //printf("\r\nD=0x%x", (unsigned int)GPIOB->ODR);
    }
    else
       GPIOB->ODR &= ~0x1;
 
    load4BitBus(regValue);
-   //printf("\r\nHO=0x%x", (regValue >> 4) & 0xf);
    pulseEnable();
    load4BitBus((regValue << 4) & 0xf0);
-   //printf("\r\nLO=0x%x", regValue  & 0xf);
    pulseEnable();
 }
 
 int lcdConfig() {
    lcd4wireHwInit();
    loadLcdRegister(FUNCTION_SET | FUNCTION_4BIT_BUS | FUNCTION_2LINE_DISPLAY, 0);
-   //printf("\r\nInst: 0x%02x", FUNCTION_SET | FUNCTION_4BIT_BUS | FUNCTION_2LINE_DISPLAY);
+   //loadLcdRegister(0x28);
 
    loadLcdRegister(DISPLAY_SETUP | DISPLAY_SETUP_ON, 0);
    //loadLcdRegister(0x0c);
-   //printf("\r\nInst: 0x%02x", DISPLAY_SETUP | DISPLAY_SETUP_ON);
 
    loadLcdRegister(ENTRY_MODE | ENTRY_MODE_INCR, 0);
    //loadLcdRegister(0x06);
-   //printf("\r\nInst: 0x%02x", ENTRY_MODE | ENTRY_MODE_INCR);
    return 0;
 }
 
@@ -185,9 +163,24 @@ void lcdInit() {
    lcdConfig();
 
    // Write a character to the display?
-   uint8_t txt[] = {'N', 'U', 'C', 'L', 'E', 'O'};
+   uint8_t txt[] = {'N', 'U', 'C', 'L', 'E', ' ', 'b', 'a', 'r', 'e', '-'};
+   uint8_t txt2[] = {'m', 'e', 't', 'a', 'l', ' ', '2', '0', '2', '4'};
    for (int i = 0; i < sizeof(txt); i++) {
       loadLcdRegister(txt[i], 1);
+      HAL_Delay(1);
+   }
+   // In a 2-line display, the cursor moves to the second line when it passes
+   // the 40th digit of the first line. Note that the first and second line
+   // displays will shift at the same time.
+   // ref. https://cdn-shop.adafruit.com/datasheets/HD44780.pdf
+   //
+   for (int i = 0; i < (40-sizeof(txt)); i++) {
+      loadLcdRegister(0x20, 1);
+      HAL_Delay(1);
+   }
+   // next line...
+   for (int i = 0; i < sizeof(txt2); i++) {
+      loadLcdRegister(txt2[i], 1);
       HAL_Delay(1);
    }
 }
--- Nucleo-F302R8/bare-metal/Debug/appver.h
+++ Nucleo-F302R8/bare-metal/Debug/appver.h
@@ -12,10 +12,10 @@
 #ifndef APPVER_H_
 #define APPVER_H_
 // The BUILD number will be updated for each build by a precompile session
-#define BUILD 322
+#define BUILD 334
 #define MAJOR_VERSION 0
 #define MINOR_VERSION 0
 // The BUILD_DATE_AND_TIME will be updated for each build by a precompile session
-#define BUILD_DATE_AND_TIME "2024-02-06 22:38:32.739200"
+#define BUILD_DATE_AND_TIME "2024-02-07 22:18:39.588737"
 
 #endif /* APPVER_H_ */
